f9c9cc0704075e8bfb1f2b116354e00c72164048
Protocol Corporation <protocollive@outlook.com>
2021-09-01 15:59:51 -0300

    Até 1589

--- src/diario/index.php
+++ src/diario/index.php
@@ -1,5 +1,5 @@
 <?php
-//2021.08.27.00
+//2021.09.01.00
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
 
@@ -7,7 +7,7 @@ function Command_diario():void{
   DebugTrace();
   global $Bot;
   $Url = 'https://raw.githubusercontent.com/SantuarioMisericordiaRJ/DiarioSantaFaustina/main';
-  $Max = 1579;
+  $Max = 1589;
   $Skip = [3, 1323, 1353, 1355];
   $Img = [1355];
   $Split = true;
